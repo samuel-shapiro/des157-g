282cac8e00a466e5994fdbf602dcc1d3053323a5
Sam Shapiro <concerningplants@gmail.com>
2019-02-05 21:09:02 -0800

    Studio 1 - Artist Name Generator Upload

--- studio1/script.js
+++ studio1/script.js
@@ -1,19 +1,26 @@
 'use strict';
 
-console.log('reading js');
+document.querySelector("form").addEventListener("submit", processForm);
 
-var favGenre = document.querySelector('#favGenre').value;
-var firstAdj = document.querySelector('#firstAdj').value;
-var favFlower = document.querySelector('#favFlower').value;
-var favColor = document.querySelector('#favColor').value;
-var momName = document.querySelector('#momName').value;
-var firstWord = document.querySelector('#firstWord').value;
+var myMsg = document.querySelector("#myMsg");
 
-var myMsg = document.querySelector('#myMsg');
+function processForm(e){
+  e.preventDefault()
 
-submit.addEventListener('click', processForm);
+  var favGenre = document.querySelector('#favGenre').value;
+  var firstAdj = document.querySelector('#firstAdj').value;
+  var favFlower = document.querySelector('#favFlower').value;
+  var favColor = document.querySelector('#favColor').value;
+  var momName = document.querySelector('#momName').value;
+  var firstWord = document.querySelector('#firstWord').value;
 
-function processForm(){
-  myMsg.innerHTML=favGenre[1] + firstAdj[2];
 
+
+  myMsg.innerHTML='The ' + favGenre.charAt(1) + favGenre.charAt(4) + 'oo'
+  + firstAdj.charAt(5) + 'y ' + momName + 's' + ' and the ' + favFlower.charAt(0)
+   + favFlower.charAt(3) + 'inky ' + favColor + ' ' + firstWord + 'es';
+
+  var formSection = document.querySelector('#formSection');
+  formSection.style.color = "white";
+  formSection.style.backgroundColor = "black";
 }
